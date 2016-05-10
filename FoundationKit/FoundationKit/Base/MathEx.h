2bcc22cee549223b4f3293875044ce747f80836c
losemymind <losemymind.libo@gmail.com>
2016-05-10 14:09:44 +0800

    FORCEINLINE functions

--- FoundationKit/FoundationKit/Base/MathEx.h
+++ FoundationKit/FoundationKit/Base/MathEx.h
@@ -26,75 +26,76 @@ namespace MathEx
 
     /// returns the maximum of two values
     template< typename T >
-    inline typename std::enable_if< std::is_arithmetic<T>::value, T >::type
-        max(T v1, T v2)
+    FORCEINLINE typename std::enable_if< std::is_arithmetic<T>::value, T >::type
+    max(T v1, T v2)
     {
         return v1 > v2 ? v1 : v2;
     }
 
     /// returns the minimum of two values
     template <typename T, typename = typename std::enable_if<std::is_arithmetic<T>::value, T>::type >
-    inline T min(T v1, T v2)
+    FORCEINLINE T min(T v1, T v2)
     {
         return v1 < v2 ? v1 : v2;
     }
 
 	/// Returns the absolute value of val.
-    template< typename T, typename std::enable_if< std::is_arithmetic<T>::value>::type* = nullptr>
-	inline T abs(T val)
-	{return std::abs(val);}
+    template< typename T>
+    FORCEINLINE T abs(T val, typename std::enable_if< std::is_arithmetic<T>::value>::type* = nullptr)
+	{
+        return std::abs(val);
+    }
 
 	/// Returns the arc-cosine of val - the angle in radians whose cosine is val.
 	template< typename T >
-	inline typename std::enable_if< std::is_arithmetic<T>::value, T >::type 
+    FORCEINLINE typename std::enable_if< std::is_arithmetic<T>::value, T >::type
 	acos(T val)
-	{return std::acos(val);}
-
-	/** 
-	 * Compares two values if they are similar.
-	 * Due to floating point imprecision it is not 
-	 * recommended to compare floats using the equal
-	 * operator. eg. 1.0 == 10.0 / 10.0 might not return true. 
-	 */
-	template< typename T1, typename T2 >
-	bool approximately(const T1 a,const T2 b,
-        typename std::enable_if< std::is_arithmetic<T1>::value >::type* = nullptr,
-        typename std::enable_if< std::is_arithmetic<T2>::value >::type* = nullptr)
-	{ return roughlyEqual(a, b, 0.1f);  }
+	{
+        return std::acos(val);
+    }
 
 	/// Returns the arc-sine of val - the angle in radians whose sine is val.
 	template< typename T >
-	inline typename std::enable_if< std::is_arithmetic<T>::value, T >::type 
+    FORCEINLINE typename std::enable_if< std::is_arithmetic<T>::value, T >::type
 	asin(const T val)
-	{ return std::asin(val); }
+	{ 
+        return std::asin(val);
+    }
 
 	/// Returns the arc-tangent of val - the angle in radians whose tangent is val.
 	template< typename T >
-	inline typename std::enable_if< std::is_arithmetic<T>::value, T >::type 
+    FORCEINLINE typename std::enable_if< std::is_arithmetic<T>::value, T >::type
 	atan(const T val)
-	{ return std::atan(val); }
+	{ 
+        return std::atan(val); 
+    }
 
 	/// Returns the angle in radians whose Tan is y/x.
 	template< typename T >
-	inline typename std::enable_if< std::is_arithmetic<T>::value, T >::type 
+    FORCEINLINE typename std::enable_if< std::is_arithmetic<T>::value, T >::type
 	atan2(const T y, const T x)
-	{ return std::atan2(y, x); }
+	{ 
+        return std::atan2(y, x); 
+    }
 
 	/// Returns the smallest integer greater to or equal to val.
 	template< typename T >
-	inline typename std::enable_if< std::is_arithmetic<T>::value, T >::type 
+    FORCEINLINE typename std::enable_if< std::is_arithmetic<T>::value, T >::type
 	ceil(const T val)
-	{ return std::ceil(val); }
+	{ 
+        return std::ceil(val); 
+    }
 
 	/// Returns the smallest integer greater to or equal to val.
 	template< typename T >
-	int ceilToInt(const T val,
-        typename std::enable_if< std::is_arithmetic<T>::value >::type* = nullptr)
-	{return static_cast<int>(std::ceil(val)); }
+    FORCEINLINE int ceilToInt(const T val, typename std::enable_if< std::is_arithmetic<T>::value >::type* = nullptr)
+	{
+        return static_cast<int>(std::ceil(val)); 
+    }
 
 	/// Returns the largest integer smaller to or equal to val.
 	template< typename T >
-	inline typename std::enable_if< std::is_arithmetic<T>::value, T >::type 
+    FORCEINLINE typename std::enable_if< std::is_arithmetic<T>::value, T >::type
 	floor(T val)
 	{
 		return std::floor(val);
@@ -102,14 +103,14 @@ namespace MathEx
 
 	/// Returns the largest integer smaller to or equal to val.
 	template< typename T >
-	int floorToInt(T val, typename std::enable_if< std::is_arithmetic<T>::value >::type* = nullptr)
+    FORCEINLINE int floorToInt(T val, typename std::enable_if< std::is_arithmetic<T>::value >::type* = nullptr)
 	{
 		return static_cast<int>(std::floor(val));
 	}
 
 	/// Clamps val value between a minimum and maximum value.
 	template< typename T>
-	inline typename std::enable_if< std::is_arithmetic<T>::value, T >::type 
+    FORCEINLINE typename std::enable_if< std::is_arithmetic<T>::value, T >::type
 	clamp(T val, T minVal, T maxVal)
 	{
         return std::min(std::max(val, minVal), maxVal);
@@ -117,7 +118,7 @@ namespace MathEx
 
 	/// Clamps value between 0 and 1 and returns value.
 	template< typename T>
-	inline typename std::enable_if< std::is_arithmetic<T>::value, T >::type 
+    FORCEINLINE typename std::enable_if< std::is_arithmetic<T>::value, T >::type
 	clamp01(T val)
 	{ 
         return std::min(std::max(val, 0), 1);
@@ -125,20 +126,95 @@ namespace MathEx
 
 	/// Returns the cosine of angle f in radians.
 	template< typename T >
-	inline typename std::enable_if< std::is_arithmetic<T>::value, T >::type 
+    FORCEINLINE typename std::enable_if< std::is_arithmetic<T>::value, T >::type
 	cos(T val)
-	{ return std::cos(val); }
+	{ 
+        return std::cos(val);
+    }
 
 	/// Returns e raised to the specified power.
 	template< typename T >
-	inline typename std::enable_if< std::is_arithmetic<T>::value, T >::type 
+    FORCEINLINE typename std::enable_if< std::is_arithmetic<T>::value, T >::type
 	exp(T power)
-	{ return std::exp(power); }
+	{ 
+        return std::exp(power);
+    }
 
+    /// Returns the natural (base e) logarithm of val specified value.
+    template< typename T >
+    FORCEINLINE typename std::enable_if< std::is_arithmetic<T>::value, T >::type
+    log(T val)
+    {
+        return std::log(val);
+    }
+
+    /// Returns the base 10 logarithm of val specified value.
+    template< typename T >
+    FORCEINLINE typename std::enable_if< std::is_arithmetic<T>::value, T >::type
+    log10(T val0)
+    {
+        return std::log10(val0);
+    }
+
+
+    /// Returns the sign of val.
+    template <typename T>
+    FORCEINLINE typename std::enable_if< std::is_arithmetic<T>::value, T >::type
+    sign(T val)
+    {
+        return (val >(T)0) ? (T)1 : ((val < (T)0) ? (T)-1 : (T)0);
+    }
+
+    /// Returns the sine of angle val in radians.
+    template <typename T>
+    FORCEINLINE typename std::enable_if< std::is_arithmetic<T>::value, T >::type
+    sin(T val)
+    {
+        return std::sin(val);
+    }
+
+    /// Returns square root of val.
+    template <typename T>
+    FORCEINLINE typename std::enable_if< std::is_arithmetic<T>::value, T >::type
+    sqrt(T val)
+    {
+        return std::sqrt(val);
+    }
+
+    /// Returns the tangent of angle val in radians.
+    template <typename T>
+    FORCEINLINE typename std::enable_if< std::is_arithmetic<T>::value, T >::type
+    tan(T val)
+    {
+        return std::tan(val);
+    }
+
+    /// Returns f raised to power p.
+    template< typename T, typename U >
+    FORCEINLINE T pow(T f, U p,
+        typename std::enable_if< std::is_arithmetic<T>::value >::type* = nullptr,
+        typename std::enable_if< std::is_arithmetic<U>::value >::type* = nullptr)
+    {
+        return std::pow(f, p);
+    }
+
+   /**
+    * Compares two values if they are similar.
+    * Due to floating point imprecision it is not
+    * recommended to compare floats using the equal
+    * operator. eg. 1.0 == 10.0 / 10.0 might not return true.
+    */
+    template< typename T1, typename T2 >
+    FORCEINLINE bool approximately(const T1 a, const T2 b,
+        typename std::enable_if< std::is_arithmetic<T1>::value >::type* = nullptr,
+        typename std::enable_if< std::is_arithmetic<T2>::value >::type* = nullptr)
+    {
+        return roughlyEqual(a, b, 0.1f);
+    }
 
 	/// Interpolates between from and to by t. t is clamped between 0 and 1.
 	template< typename T, typename U, typename V>
-	inline float Lerp(T from, U to, V t,
+    FORCEINLINE float Lerp(T from, U to, V t,
         typename std::enable_if< std::is_arithmetic<T>::value >::type* = nullptr,
         typename std::enable_if< std::is_arithmetic<U>::value >::type* = nullptr,
         typename std::enable_if< std::is_arithmetic<V>::value >::type* = nullptr)
@@ -146,40 +222,23 @@ namespace MathEx
 		return t >= 1 ? to : t < 0 ? from : from + (to - from) * t; 
 	}
 
-	/// Returns the natural (base e) logarithm of val specified value.
-	template< typename T >
-	inline typename std::enable_if< std::is_arithmetic<T>::value, T >::type 
-	log(T val)
-	{ return std::log(val); }
-
-	/// Returns the base 10 logarithm of val specified value.
-	template< typename T >
-	inline typename std::enable_if< std::is_arithmetic<T>::value, T >::type 
-	log10(T val0)
-	{ return std::log10(val0); }
-
-	/// Returns f raised to power p.
-	template< typename T , typename U >
-	T pow(T f, U p,
-        typename std::enable_if< std::is_arithmetic<T>::value >::type* = nullptr,
-        typename std::enable_if< std::is_arithmetic<U>::value >::type* = nullptr)
-	{ return std::pow(f, p); }
-
-	/**
-		* Compares two floating point values if they are similar.
-		* @param[in] v1 First value.
-		* @param[in] v2 Second value.
-		* @param[in] threshold The threshold of similarity
-		* @return True if the values are similar, otherwise false.
-		*/
+   /**
+    * Compares two floating point values if they are similar.
+    * @param[in] v1 First value.
+    * @param[in] v2 Second value.
+    * @param[in] threshold The threshold of similarity
+    * @return True if the values are similar, otherwise false.
+    */
 	template< typename T, typename U, typename V>
-	bool roughlyEqual(const T v1, const U v2, const V threshold = 0.01f) 
-	{ return std::abs(v1 - v2) <= threshold;  }
+    FORCEINLINE bool roughlyEqual(const T v1, const U v2, const V threshold = 0.01f)
+	{ 
+        return std::abs(v1 - v2) <= threshold;  
+    }
 
 	/// Returns f rounded to the nearest integer.
     template <typename T>
-    T round(T val, 
-        typename std::enable_if< std::is_arithmetic<T>::value >::type* = nullptr)
+    FORCEINLINE typename std::enable_if< std::is_arithmetic<T>::value, T >::type
+    round(T val)
     {
 #if TARGET_PLATFORM == PLATFORM_ANDROID
         return (val > T(0)) ? std::floor(val + T(0.5)) : std::ceil(val - T(0.5));
@@ -188,33 +247,77 @@ namespace MathEx
 #endif
     }
 
-	/// Returns the sign of val.
-    template <typename T>
-    inline typename std::enable_if< std::is_arithmetic<T>::value, T >::type
-    sign(T val)
-    { 
-        if (val > T(0)) return T(1);
-        if (val < T(0)) return T(-1);
-        return T(0);
+    FORCEINLINE bool equal(const float& v1, const float& v2)
+    {
+        return (std::fabs(v1 - v2) < 1E-12);
     }
 
-	/// Returns the sine of angle val in radians.
-	template <typename T>
-	inline typename std::enable_if< std::is_arithmetic<T>::value, T >::type 
-	sin(T val)
-	{ return std::sin(val); }
+    FORCEINLINE bool equal(const double& v1, const double& v2)
+    {
+        return (std::fabs(v1 - v2) < 1E-12);
+    }
 
-	/// Returns square root of val.
-	template <typename T>
-	inline typename std::enable_if< std::is_arithmetic<T>::value, T >::type 
-	sqrt(T val)
-	{ return std::sqrt(val); }
+    template< typename T >
+    FORCEINLINE bool fuzzyEqual(T v1, T v2, T var)
+    {
+        return(v1 - var <= v2 && v2 <= v1 + var);
+    }
 
-	/// Returns the tangent of angle val in radians.
-	template <typename T>
-	inline typename std::enable_if< std::is_arithmetic<T>::value, T >::type 
-	tan(T val)
-	{ return std::tan(val); }
+    /// Return true if the parameter is equal to zero
+    FORCEINLINE bool isZero(double val)
+    {
+        return ((-MinDouble < val) && (val < MinDouble));
+    }
+
+    /// Returns if the value is powered by two.
+    FORCEINLINE bool isPowerOfTwo(int val)
+    {
+        return (val > 0) && ((val & (val - 1)) == 0);
+    }
+
+    FORCEINLINE double sigmoid(double input, double response = 1.0)
+    {
+        return (1.0 / (1.0 + exp(-input / response)));
+    }
+
+    // return true is the third parameter is in the range described by the first two
+    template< typename T, typename U, typename V>
+    FORCEINLINE bool inRange(T start, U end, V val)
+    {
+        if (start < end)
+        {
+            return ((val > start) && (val < end));
+        }
+        else
+        {
+            return ((val < start) && (val > end));
+        }
+    }
+
+
+   /** Converts radians to degrees.
+    *  This method uses double precission internally,
+    *  though it returns single float
+    *  Factor = 180 / pi
+    *  @param[in] radians The angle in radians.
+    *  @return    The angle in degrees.
+    */
+    FORCEINLINE float radiansToDegrees(float radians)
+    {
+        return static_cast<float>(radians * 57.295779513082320876798154814105);
+    }
+
+   /** Converts degrees to radians.
+    *  This method uses double precission internally,
+    *  though it returns single float
+    *  Factor = pi / 180
+    *  @param[in]  degrees  The angle in degrees.
+    *  @return     The angle in radians.
+    */
+    FORCEINLINE float degreesToRadians(float degrees)
+    {
+        return static_cast<float>(degrees * 0.017453292519943295769236907684886);
+    }
 
 	template<typename T>
 	double average(const std::vector<T>& vec,
@@ -226,78 +329,6 @@ namespace MathEx
 			dAverage += static_cast<double>(i);
 		}
 		return dAverage / static_cast<double>(vec.size());
-	}
-
-	inline bool equal(const float& v1, const float& v2)
-	{
-		return (std::fabs(v1-v2) < 1E-12);
-	}
-
-	inline bool equal(const double& v1, const double& v2)
-	{
-		return (std::fabs(v1-v2) < 1E-12);
-	}
-
-	template< typename T >
-	inline bool fuzzyEqual(T v1, T v2, T var)
-	{
-		return(v1 - var <= v2 && v2 <= v1 + var);
-	}
-
-	/// Return true if the parameter is equal to zero
-	inline bool isZero(double val)
-	{
-		return ((-MinDouble < val) && (val < MinDouble));
-	}
-
-	/// Returns if the value is powered by two.
-	inline bool isPowerOfTwo(int val) 
-	{
-		return (val > 0) && ((val & (val - 1)) == 0);
-	}
-
-	inline double sigmoid(double input, double response = 1.0)
-	{
-		return ( 1.0 / ( 1.0 + exp(-input / response)));
-	}
-
-	// return true is the third parameter is in the range described by the first two
-	template< typename T, typename U, typename V>
-	inline bool inRange(T start, U end, V val)
-	{
-		if (start < end)
-		{
-			return ((val > start) && (val < end));
-		}
-		else
-		{
-			return ((val < start) && (val > end));
-		}
-	}
-
-
-	/** Converts radians to degrees.
-	 *  This method uses double precission internally,
-	 *  though it returns single float
-	 *  Factor = 180 / pi
-	 *  @param[in] radians The angle in radians.
-	 *  @return    The angle in degrees.
-	 */
-	inline float radiansToDegrees(float radians)
-	{ 
-		return static_cast<float>(radians * 57.295779513082320876798154814105);
-	}
-
-	/** Converts degrees to radians.
-	 *  This method uses double precission internally,
-	 *  though it returns single float
-	 *  Factor = pi / 180
-	 *  @param[in]  degrees  The angle in degrees.
-	 *  @return     The angle in radians.
-	 */
-	inline float degreesToRadians(float degrees)
-	{ 
-		return static_cast<float>(degrees * 0.017453292519943295769236907684886);
 	}
 
 	/// Get the next power of two after val value.
