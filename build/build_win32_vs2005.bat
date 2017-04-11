fc940cfd128ae2bf2a3fc8250682a8387acf792a
losemymind <losemymind.libo@gmail.com>
2017-04-11 19:09:18 +0800

    no message

--- build/build_win32_vs2005.bat
+++ build/build_win32_vs2005.bat
@@ -15,11 +15,14 @@ call "%DEVENV%" ..\FoundationKitCPP03\proj.win32\FoundationKitCPP03.sln /rebuild
 ::call "%DEVENV%" ..\FoundationKitCPP03\proj.win32\FoundationKitCPP03.sln /rebuild "Debug|win32"
 ::call "%DEVENV%" ..\FoundationKitCPP03\proj.win32\FoundationKitCPP03.sln /rebuild "Release|win32"
 
+if exist "..\publish\vs2005\" rmdir "..\publish\vs2005\" /s /q
 
+xcopy /Y /Q /S "..\FoundationKitCPP03\FoundationKit\*.h" "..\publish\vs2005\include\FoundationKit\"
+xcopy /Y /Q /S "..\FoundationKitCPP03\FoundationKit\*.hpp" "..\publish\vs2005\include\FoundationKit\"
+xcopy /Y /Q /S "..\FoundationKitCPP03\FoundationKit\*.inl" "..\publish\vs2005\include\FoundationKit\"
 
-xcopy /Y /Q /S "..\FoundationKitCPP03\FoundationKit\*.h" "..\publish\vs2005\FoundationKit\"
-xcopy /Y /Q /S "..\FoundationKitCPP03\FoundationKit\*.hpp" "..\publish\vs2005\FoundationKit\"
-xcopy /Y /Q /S "..\FoundationKitCPP03\FoundationKit\*.inl" "..\publish\vs2005\FoundationKit\"
+xcopy /Y /Q /S "..\FoundationKitCPP03\proj.win32\Debug\*.lib" "..\publish\vs2005\libs\Debug\"
+xcopy /Y /Q /S "..\FoundationKitCPP03\proj.win32\Release\*.lib" "..\publish\vs2005\libs\Release\"
 
 goto :eof
 :END
---
+++ build/build_win32_vs2015.bat
@@ -0,0 +1,36 @@
+@echo off
+REM
+REM
+
+%~d0
+cd %~dp0
+
+set DEVENV=%VS140COMNTOOLS%..\IDE\devenv
+
+if exist "..\Win32" rmdir "..\Win32" /s /q
+::Build project FoundationKit
+call "%DEVENV%" ..\FoundationKitFramework.sln /rebuild "Debug|win32" /project FoundationKit /projectconfig "Debug|win32"
+call "%DEVENV%" ..\FoundationKitFramework.sln /rebuild "Release|win32" /project FoundationKit /projectconfig "Release|win32"
+
+::Build Solution FoundationKitCPP03.sln
+::call "%DEVENV%" ..\FoundationKitCPP03\proj.win32\FoundationKitCPP03.sln /rebuild "Debug|win32"
+::call "%DEVENV%" ..\FoundationKitCPP03\proj.win32\FoundationKitCPP03.sln /rebuild "Release|win32"
+
+set PUBLISH_DIR="..\publish\vs2015\"
+
+if exist %PUBLISH_DIR% rmdir %PUBLISH_DIR% /s /q
+
+xcopy /Y /Q /S "..\FoundationKit\FoundationKit\*.h" "%PUBLISH_DIR%include\FoundationKit\"
+xcopy /Y /Q /S "..\FoundationKit\FoundationKit\*.hpp" "%PUBLISH_DIR%include\FoundationKit\"
+xcopy /Y /Q /S "..\FoundationKit\FoundationKit\*.inl" "%PUBLISH_DIR%include\FoundationKit\"
+
+xcopy /Y /Q /S "..\Win32\Debug\*.lib" "%PUBLISH_DIR%libs\Debug\"
+xcopy /Y /Q /S "..\Win32\Release\*.lib" "%PUBLISH_DIR%libs\Release\"
+
+xcopy /Y /Q /S "..\Win32\Debug\*.dll" "%PUBLISH_DIR%libs\Debug\"
+xcopy /Y /Q /S "..\Win32\Release\*.dll" "%PUBLISH_DIR%libs\Release\"
+
+
+goto :eof
+:END
+echo %1
