4eb5c3d6750850c1363398a6a969844efb7c6a1d
losemymind <losemymind.libo@gmail.com>
2017-04-17 16:53:14 +0800

    add ios IsDebuggerPresent

--- FoundationKit/FoundationKit/Platform/ios/PlatformIos.mm
+++ FoundationKit/FoundationKit/Platform/ios/PlatformIos.mm
@@ -10,7 +10,15 @@
 #include "FoundationKit/Platform/Platform.hpp"
 
 NS_FK_BEGIN
-
+bool Platform::IsDebuggerPresent()
+{
+    // Based on http://developer.apple.com/library/mac/#qa/qa1361/_index.html
+	struct kinfo_proc Info;
+	int32 Mib[] = { CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid() };
+	size_t Size = sizeof(Info);
+	sysctl( Mib, sizeof( Mib ) / sizeof( *Mib ), &Info, &Size, NULL, 0 );
+	return ( Info.kp_proc.p_flag & P_TRACED ) != 0;
+}
 NS_FK_END
 
 #endif //TARGET_PLATFORM == PLATFORM_IOS
