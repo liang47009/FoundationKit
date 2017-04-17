cc50ef4f2d21f38cae00143bc03bbd01c128acd1
losemymind <losemymind.libo@gmail.com>
2017-04-17 16:34:36 +0800

    no message

--- FoundationKit/FoundationKit/Foundation/Logger.hpp
+++ FoundationKit/FoundationKit/Foundation/Logger.hpp
@@ -8,83 +8,73 @@
 #define FOUNDATIONKIT_LOGGER_HPP
 
 #pragma once
+#include "FoundationKit/forward.hpp"
 #include "FoundationKit/Foundation/Singleton.hpp"
-#include <cassert>
+#include "spdlog/spdlog.h"
+#if (TARGET_PLATFORM == PLATFORM_ANDROID)
+#include <android/log.h>
+#elif (TARGET_PLATFORM == PLATFORM_WINDOWS)
+#include <Windows.h>
+#endif
+
+
 NS_FK_BEGIN
 
 class Logger : public Singleton<Logger>
 {
     friend class Singleton < Logger >;
-    Logger(){};
+    Logger();
 public:
-    /** 
-     * Enumeration of valid log levels.
-     */
-    enum Level
-    {
-        LV_VERBOSE,
-        LV_DEBUG,
-        LV_INFO,
-        LV_WARN,
-        LV_ERROR,
-        LV_ASSERT,
-    };
-private:
-    struct State
-    {
-        State();
-        bool enabled;
-    };
-
+    using pointer = std::shared_ptr<spdlog::logger>;
 public:
     virtual~Logger();
-    /**
-     * Logs a message at the specified log level.
-     *
-     * This method accepts a variable argument list with the same formatting specification
-     * as printf. Therefore, the message parameter can include any format specifiers that
-     * are supported by printf.
-     *
-     * @param level Log level.
-     * @param message Log message.
-     */
-    void log(Level level, const char* message, ...);
-
-    /**
-     * Determines if logging is currently enabled for the given level.
-     *
-     * @param level Log level.
-     *
-     * @return True if logging is enabled for this level, or false if it is disabled.
-     */
-    bool isEnabled(Level level);
-
-    /**
-     * Enables or disables logging at the given level.
-     *
-     * @param level Log level to enable or disable.
-     * @param enabled True to enable the logger for the given level, false to disable it.
-     */
-    void setEnabled(Level level, bool enabled);
+    bool init(const std::string& fileName);
+    pointer get_log();
+    pointer get_console_log();
 private:
-    State _states[6];
+    pointer _log;
+    pointer _console_log;
 };
+
+template<typename... Args>
+static inline void LOG_TRACE(const char* fmt, const Args&... args)
+{
+    Logger::getInstance()->get_log()->trace(fmt, args...);
+}
+
+template<typename... Args>
+static inline void LOG_INFO(const char* fmt, const Args&... args)
+{
+    Logger::getInstance()->get_log()->info(fmt, args...);
+}
+
+template<typename... Args>
+static inline void LOG_WARN(const char* fmt, const Args&... args)
+{
+    Logger::getInstance()->get_log()->warn(fmt, args...);
+}
+
+template<typename... Args>
+static inline void LOG_ERROR(const char* fmt, const Args&... args)
+{
+    Logger::getInstance()->get_log()->error(fmt, args...);
+}
+
+template<typename... Args>
+static inline void LOG_CRITICAL(const char* fmt, const Args&... args)
+{
+    Logger::getInstance()->get_log()->critical(fmt, args...);
+}
+
+template<typename... Args>
+static inline void LOG_DEBUG(const char* fmt, const Args&... args)
+{
+    Logger::getInstance()->get_log()->set_level(spdlog::level::debug);
+    Logger::getInstance()->get_log()->debug(fmt, args...);
+    Logger::getInstance()->get_console_log()->set_level(spdlog::level::debug);
+    Logger::getInstance()->get_console_log()->debug(fmt, args...);
+}
+
 NS_FK_END
 
-#if DEBUG_MODE
-#define LOG_DEBUG(format, ...) NS_NAME::Logger::getInstance()->log(NS_NAME::Logger::Level::LV_DEBUG, format, ##__VA_ARGS__)
-#else
-#define LOG_DEBUG(format, ...)
-#endif
-#define LOG_INFO( format, ...) NS_NAME::Logger::getInstance()->log(NS_NAME::Logger::Level::LV_INFO, format, ##__VA_ARGS__)
-#define LOG_WARN( format, ...) NS_NAME::Logger::getInstance()->log(NS_NAME::Logger::Level::LV_WARN, format, ##__VA_ARGS__)
-#define LOG_ERROR(format, ...) NS_NAME::Logger::getInstance()->log(NS_NAME::Logger::Level::LV_ERROR, format, ##__VA_ARGS__)
-#define LOG_ASSERT(cond, format,...) \
-do{ \
-    if (!(cond)) { \
-        NS_NAME::Logger::getInstance()->log(NS_NAME::Logger::Level::LV_ASSERT, format, ##__VA_ARGS__); \
-        assert(cond); \
-        } \
-} \
-while (false)
 #endif // FOUNDATIONKIT_LOGGER_HPP
