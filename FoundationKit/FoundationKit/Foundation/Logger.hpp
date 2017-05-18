bb32561824336583c55f6ae24568e88aee48598c
losemymind <losemymind.libo@gmail.com>
2017-05-18 18:14:28 +0800

    no message

--- FoundationKit/FoundationKit/Foundation/Logger.hpp
+++ FoundationKit/FoundationKit/Foundation/Logger.hpp
@@ -10,6 +10,7 @@
 #pragma once
 #include "FoundationKit/forward.hpp"
 #include "FoundationKit/Foundation/Singleton.hpp"
+#include "FoundationKit/Platform/Platform.hpp"
 #include "spdlog/spdlog.h"
 #if (TARGET_PLATFORM == PLATFORM_ANDROID)
 #include <android/log.h>
@@ -20,6 +21,17 @@
 
 NS_FK_BEGIN
 
+typedef enum
+{
+    trace = 0,
+    debug = 1,
+    info = 2,
+    warn = 3,
+    err = 4,
+    critical = 5,
+    off = 6
+} LOG_LEVEL_ENUM;
+
 class Logger : public Singleton<Logger>
 {
     friend class Singleton < Logger >;
@@ -29,8 +41,10 @@ public:
 public:
     virtual~Logger();
     bool init(const std::string& fileName);
-    pointer get_log();
-    pointer get_console_log();
+    void setLogLevel(LOG_LEVEL_ENUM logLevel);
+    void setConsoleLogLevel(LOG_LEVEL_ENUM logLevel);
+    pointer getLog();
+    pointer getConsoleLog();
 private:
     pointer _log;
     pointer _console_log;
@@ -39,38 +53,37 @@ private:
 template<typename... Args>
 static inline void LOG_TRACE(const char* fmt, const Args&... args)
 {
-    Logger::getInstance()->get_log()->trace(fmt, args...);
+    Logger::getInstance()->getLog()->trace(fmt, args...);
 }
 
 template<typename... Args>
 static inline void LOG_INFO(const char* fmt, const Args&... args)
 {
-    Logger::getInstance()->get_log()->info(fmt, args...);
+    Logger::getInstance()->getLog()->info(fmt, args...);
 }
 
 template<typename... Args>
 static inline void LOG_WARN(const char* fmt, const Args&... args)
 {
-    Logger::getInstance()->get_log()->warn(fmt, args...);
+    Logger::getInstance()->getLog()->warn(fmt, args...);
 }
 
 template<typename... Args>
 static inline void LOG_ERROR(const char* fmt, const Args&... args)
 {
-    Logger::getInstance()->get_log()->error(fmt, args...);
+    Logger::getInstance()->getLog()->error(fmt, args...);
 }
 
 template<typename... Args>
 static inline void LOG_CRITICAL(const char* fmt, const Args&... args)
 {
-    Logger::getInstance()->get_log()->critical(fmt, args...);
+    Logger::getInstance()->getLog()->critical(fmt, args...);
 }
 
 template<typename... Args>
 static inline void LOG_DEBUG(const char* fmt, const Args&... args)
 {
-    Logger::getInstance()->get_console_log()->set_level(spdlog::level::debug);
-    Logger::getInstance()->get_console_log()->debug(fmt, args...);
+    Logger::getInstance()->getConsoleLog()->debug(fmt, args...);
 }
 
 NS_FK_END
