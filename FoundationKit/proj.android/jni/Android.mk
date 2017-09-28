8e0e58fb238654b41b81d1666d6b27622a95dc32
losemymind <losemymind.libo@gmail.com>
2017-09-28 19:12:50 +0800

    no message

--- FoundationKit/proj.android/jni/Android.mk
+++ FoundationKit/proj.android/jni/Android.mk
@@ -8,8 +8,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 SRCROOT := ../..
-PROJECT_DIR=$(LOCAL_PATH)/../..
-
+PROJECT_DIR:=$(LOCAL_PATH)/../..
 LOCAL_MODULE := foundationkit
 LOCAL_MODULE_FILENAME := libfoundationkit
 LOCAL_SRC_FILES := \
@@ -57,12 +56,14 @@ $(PROJECT_DIR)/ \
 $(PROJECT_DIR)/FoundationKit/external/unzip \
 $(PROJECT_DIR)/../ThirdParty/spdlog/include
 
-TARGET_LOCAL_CFLAGS := \
--fexceptions
+TARGET_LOCAL_CFLAGS:= \
+-fsigned-char \
+-std=c++14 \
+-Wno-deprecated-declarations \
+-DUSE_FILE32API \
+-DANDROID
 
 TARGET_LOCAL_CPPFLAGS := \
--fexceptions \
--frtti \
 -fsigned-char \
 -std=c++14 \
 -Wno-deprecated-declarations \
@@ -78,21 +79,11 @@ TARGET_LOCAL_LDLIBS := \
 -lz \
 -lGLESv2
 
-ifeq ($(NDK_DEBUG),1)
-	TARGET_LOCAL_CFLAGS += -DDEBUG
-	TARGET_LOCAL_CPPFLAGS += -DDEBUG
-else
-	TARGET_LOCAL_CFLAGS += -DNDEBUG
-	TARGET_LOCAL_CPPFLAGS += -DNDEBUG
-endif
-
-ifeq (armeabi-v7a,$(TARGET_ARCH_ABI))
-	TARGET_LOCAL_CPPFLAGS += -DUSE_NEON
-endif
+TARGET_LOCAL_CFLAGS   += -DNDEBUG
+TARGET_LOCAL_CPPFLAGS += -DNDEBUG
 
 ifeq ($(ENABLE_ADDRESS_SANITIZER),1)
 # TARGET_LOCAL_CPPFLAGS += -fsanitize=address -fno-omit-frame-pointer -O1
-# TARGET_LOCAL_CFLAGS   += -fsanitize=address -fno-omit-frame-pointer -O1
 # TARGET_LOCAL_LDFLAGS  += -fsanitize=address -O1
 # LOCAL_SANITIZE:=unsigned-integer-overflow signed-integer-overflow
 # LOCAL_SANITIZE_DIAG:=unsigned-integer-overflow signed-integer-overflow
@@ -100,23 +91,21 @@ endif
 
 ifeq ($(REDUCE_BIN_SIZE),1)
 # TARGET_LOCAL_CPPFLAGS += -fvisibility=hidden
-# TARGET_LOCAL_CFLAGS   += -fvisibility=hidden 
 # TARGET_LOCAL_CPPFLAGS += -ffunction-sections -fdata-sections
-# TARGET_LOCAL_CFLAGS   += -ffunction-sections -fdata-sections
 endif
 
 ################## COMPILE CONFIG ###################
 
 LOCAL_SHORT_COMMANDS := true
+LOCAL_CPP_FEATURES := rtti exceptions
 
-LOCAL_CFLAGS   := $(TARGET_LOCAL_CFLAG)
+LOCAL_CFLAGS:= $(TARGET_LOCAL_CFLAGS)
 LOCAL_CPPFLAGS := $(TARGET_LOCAL_CPPFLAGS)
 #LOCAL_LDFLAGS  := $(TARGET_LOCAL_LDFLAGS)
 #LOCAL_LDLIBS   := $(TARGET_LOCAL_LDLIBS)
 
-LOCAL_STATIC_LIBRARIES += cpufeatures
-
-LOCAL_EXPORT_CFLAGS := $(TARGET_LOCAL_CFLAG)
+LOCAL_STATIC_LIBRARIES := cpufeatures
+LOCAL_EXPORT_CFLAGS := $(TARGET_LOCAL_CFLAGS)
 LOCAL_EXPORT_CPPFLAGS := $(TARGET_LOCAL_CPPFLAGS)
 LOCAL_EXPORT_LDLIBS := $(TARGET_LOCAL_LDLIBS)
 LOCAL_EXPORT_LDFLAGS := $(TARGET_LOCAL_LDFLAGS)
@@ -127,15 +116,12 @@ include $(BUILD_STATIC_LIBRARY)
 #==============================================================
 $(call import-add-path,$(PROJECT_DIR)/FoundationKit)
 $(call import-module,android/cpufeatures)
-
 $(info ----------------- Compile libfoundationkit infomation -------------------)
 $(info TARGET_PLATFORM = $(TARGET_PLATFORM))
 $(info TARGET_ARCH     = $(TARGET_ARCH))
 $(info TARGET_ARCH_ABI = $(TARGET_ARCH_ABI))
 $(info TARGET_ABI      = $(TARGET_ABI))
 $(info APP_ABI         = $(APP_ABI))
-$(info NDK_DEBUG       = $(NDK_DEBUG))
-$(info SRCROOT         = $(SRCROOT))
 $(info --------------------------------------------------------------------------)
 
 
