43ea7462c1dfdec5f81a212ca7e5be47a9286071
losemymind <losemymind.libo@gmail.com>
2017-12-07 11:53:34 +0800

    no message

--- build/build_win32_vs2005.bat
+++ build/build_win32_vs2005.bat
@@ -11,10 +11,6 @@ set DEVENV=%VS80COMNTOOLS%..\IDE\devenv
 call "%DEVENV%" ..\FoundationKitCPP03\proj.win32\FoundationKitCPP03.sln /rebuild "Debug|win32" /project FoundationKit /projectconfig "Debug|win32"
 call "%DEVENV%" ..\FoundationKitCPP03\proj.win32\FoundationKitCPP03.sln /rebuild "Release|win32" /project FoundationKit /projectconfig "Release|win32"
 
-::Build Solution FoundationKitCPP03.sln
-::call "%DEVENV%" ..\FoundationKitCPP03\proj.win32\FoundationKitCPP03.sln /rebuild "Debug|win32"
-::call "%DEVENV%" ..\FoundationKitCPP03\proj.win32\FoundationKitCPP03.sln /rebuild "Release|win32"
-
 if exist "..\publish\vs2005\" rmdir "..\publish\vs2005\" /s /q
 
 xcopy /Y /Q /S "..\FoundationKitCPP03\FoundationKit\*.h" "..\publish\vs2005\include\FoundationKit\"
