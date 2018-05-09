fa6aef6455d044ec8ff2afdf4a69073a4b2207e6
losemymind <losemymind.libo@gmail.com>
2018-05-09 10:56:43 +0800

    no message

--- FoundationKit/FoundationKit/FoundationKitVersion.hpp
+++ FoundationKit/FoundationKit/FoundationKitVersion.hpp
@@ -13,7 +13,6 @@
 #define FOUNDATIONKIT_VERSION_REVISION 0
 #define FOUNDATIONKIT_VERSION_FULL "1.0.0.0"
 
-
 // Version as a single hex number, e.g. 0x01000300 == 1.0.3
 #define FOUNDATIONKIT_VERSION_HEX ((FOUNDATIONKIT_VERSION_MAJOR << 24) | \
                                    (FOUNDATIONKIT_VERSION_MINOR << 16) | \
