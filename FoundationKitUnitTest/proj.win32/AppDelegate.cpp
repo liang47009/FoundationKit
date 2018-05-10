c74e2daa3ef5d37f4eeb1af42f290e5d168edada
losemymind <losemymind.libo@gmail.com>
2018-05-10 11:38:56 +0800

    NO MESSAGE

--- FoundationKitUnitTest/proj.win32/AppDelegate.cpp
+++ FoundationKitUnitTest/proj.win32/AppDelegate.cpp
@@ -46,7 +46,6 @@
 #include "FoundationKit/Platform/File.hpp"
 #include "FoundationKit/Platform/Path.hpp"
 #include "FoundationKit/Platform/PlatformDevice.hpp"
-#include "FoundationKit/Platform/PlatformMemory.hpp"
 #include "FoundationKit/Platform/PlatformMisc.hpp"
 #include "FoundationKit/Platform/PlatformTLS.hpp"
 
