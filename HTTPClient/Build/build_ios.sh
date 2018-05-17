49a15bde22c99700dd715fdae717c15754e97296
losemymind <losemymind.libo@gmail.com>
2018-05-17 16:37:18 +0800

    NO MESSAGE

--- HTTPClient/Build/build_ios.sh
+++ HTTPClient/Build/build_ios.sh
@@ -4,6 +4,6 @@ mkdir projects
 cd projects
 mkdir proj.ios
 cd proj.ios
-cmake -DCMAKE_TOOLCHAIN_FILE=../../CMake/ToolChains/ios.toolchain.cmake -DIOS_PLATFORM=OS -DIOS_BITCODE=FALSE ../../ 
+cmake -DCMAKE_TOOLCHAIN_FILE=../../CMake/ToolChains/ios.toolchain.cmake -DIOS_PLATFORM=OS -DENABLE_BITCODE=0 ../../ 
 make
 make install
